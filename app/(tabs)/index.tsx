fcd9041dbd05b456e07300dfcea18ad21e7d99ba
jkferland <joesephferland@gmail.com>
2024-10-22 10:31:58 -0700

    feat: Added top nav

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { SafeAreaView, View } from 'react-native';
+import { SafeAreaView, View, TouchableOpacity } from 'react-native';
 import { Text } from '~/components/nativewindui/Text';
 import {
   Card,
@@ -22,19 +22,24 @@ export default function Screen() {
 
   return (
     <SafeAreaView className="flex-1">
-      <TopNav
-        title="Sizzle"
-        onButton1Press={handleButton1Press}
-        onButton2Press={handleButton2Press}
-      />
-      <View className="flex-1 justify-center mx-4">
+      <TopNav title="Welcome">
+        <TouchableOpacity onPress={handleButton1Press} className="mr-4">
+          <Text className="text-blue-500">Button 1</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleButton2Press}>
+          <Text className="text-blue-500">Button 2</Text>
+        </TouchableOpacity>
+      </TopNav>
+      <View className="mx-4 flex-1 justify-center">
         <Card>
           <CardContent>
             <CardTitle>Welcome to Sizzle</CardTitle>
             <CardSubtitle>Version 1.0.0</CardSubtitle>
           </CardContent>
           <CardFooter>
-            <CardDescription>Sizzle is a boilerplate app for building apps with NativeWindUI</CardDescription>
+            <CardDescription>
+              Sizzle is a boilerplate app for building apps with NativeWindUI
+            </CardDescription>
           </CardFooter>
         </Card>
       </View>
--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import { View } from 'react-native';
 import { Text } from '~/components/nativewindui/Text';
 
 interface TopNavProps {
   title: string;
-  onButton1Press: () => void;
-  onButton2Press: () => void;
+  children?: React.ReactNode;
 }
 
-export function TopNav({ title, onButton1Press, onButton2Press }: TopNavProps) {
+export function TopNav({ title, children }: TopNavProps) {
   return (
     <View className="flex-row justify-between items-center p-4 bg-gray-100">
-      <Text className="ios:font-['Inter']">{title}</Text>
+      <Text className="ios:font-['Inter'] font-light text-2xl">{title}</Text>
       <View className="flex-row">
-        <TouchableOpacity onPress={onButton1Press} className="mr-4">
-          <Text className="text-blue-500">Button 1</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={onButton2Press}>
-          <Text className="text-blue-500">Button 2</Text>
-        </TouchableOpacity>
+        {children}
       </View>
     </View>
   );
 }
-
