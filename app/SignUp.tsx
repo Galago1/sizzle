7ff499e48ce41d17102596f22dfbb168dde05367
jkferland <joesephferland@gmail.com>
2024-10-23 10:11:34 -0700

    feat: Fixed signup text

--- app/SignUp.tsx
+++ app/SignUp.tsx
@@ -22,8 +22,8 @@ export default function ForgotPasswordScreen() {
         <MaterialIcons name="arrow-back" size={24} color="black" />
       </TouchableOpacity>
       <MaterialIcons name="login" size={24} color="black" />
-      <Text className="mt-6 font-['Inter'] text-2xl text-gray-600">Create Account </Text>
-      <Text className="mb-4 font-['Inter'] text-sm text-gray-600">
+      <Text className="mt-6 font-['Inter'] text-2xl font-light text-gray-600">Create Account </Text>
+      <Text className="mb-4 font-['Inter'] text-sm font-normal text-gray-600">
         Start your journey to becoming your best self
       </Text>
 
@@ -31,6 +31,7 @@ export default function ForgotPasswordScreen() {
         className="rounded-xl bg-white font-['Inter']"
         label="Name"
         containerClassName="mb-4"
+        labelClassName="font-normal text-gray-600 text-sm"
         placeholder="Enter your name"
         leftView={<Feather name="user" size={24} color="gray" />}
       />
@@ -38,11 +39,13 @@ export default function ForgotPasswordScreen() {
         className="rounded-xl bg-white font-['Inter']"
         label="Email"
         containerClassName="mb-4"
+        labelClassName="font-normal text-gray-600 text-sm"
         placeholder="Enter your email"
         leftView={<MaterialIcons name="email" size={20} color="gray" />}
       />
       <TextField
         className="rounded-xl bg-white font-['Inter']"
+        labelClassName="font-normal text-gray-600 text-sm"
         label="Password"
         placeholder="Enter a password"
         secureTextEntry
@@ -55,29 +58,29 @@ export default function ForgotPasswordScreen() {
             {' '}
             By continuing, you agree to our{' '}
           </Text>
-          <TouchableOpacity onPress={() => router.push('/SignUp')}>
-            <Text className="font-['Inter'] text-xs font-semibold text-gray-600 underline">
+          <TouchableOpacity>
+            <Text className="font-['Inter'] text-xs font-normal text-gray-600 underline">
               Terms of Service
             </Text>
           </TouchableOpacity>
         </View>
         <View className="mb-1 flex-row justify-center">
           <Text className="font-['Inter'] text-xs text-gray-600"> & </Text>
-          <TouchableOpacity onPress={() => router.push('/SignUp')}>
-            <Text className="font-['Inter'] text-xs font-semibold text-gray-600 underline">
+          <TouchableOpacity>
+            <Text className="font-['Inter'] text-xs font-normal text-gray-600 underline">
               Privacy Policy
             </Text>
           </TouchableOpacity>
         </View>
         <TouchableOpacity className="rounded-xl bg-gray-600 py-3">
-          <Text className="text-center font-['Inter'] font-semibold text-white">
+          <Text className="text-center font-['Inter'] font-normal text-white">
             Create My Account
           </Text>
         </TouchableOpacity>
         <View className="mt-4 flex-row justify-center">
-          <Text className="font-['Inter'] text-xs text-gray-600">Already have an account? </Text>
-          <TouchableOpacity onPress={() => router.push('/SignUp')}>
-            <Text className="font-['Inter'] text-sm font-semibold text-gray-600 underline">
+          <Text className="font-['Inter'] text-sm text-gray-600">Already have an account? </Text>
+          <TouchableOpacity onPress={() => router.push('/login')}>
+            <Text className="font-['Inter'] text-sm font-normal text-gray-600 underline">
               Login
             </Text>
           </TouchableOpacity>
