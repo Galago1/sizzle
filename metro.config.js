fbe53ea28afc9ded0919a1450b2d37a757168ffe
jkferland <joesephferland@gmail.com>
2024-10-25 16:22:40 -0700

    feat: Halfway fixed stories

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,12 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const withStorybook = require('@storybook/react-native/metro/withStorybook');
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
+const { generate } = require('@storybook/react-native/scripts/generate');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 // eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname);
 
-module.exports = withStorybook(withNativeWind(config, { input: './global.css', inlineRem: 16 }));
+config.transformer.unstable_allowRequireContext = true;
+
+module.exports = withNativeWind(config, { input: './global.css', inlineRem: 16 });
